292da41730dd0b6a4ab7e34faef97b298d9d707c
hallllow29 <8230068@estg.ipp.pt>
2024-11-29 14:21:36 +0000

    Reading from json file with the help of https://mkyong.com/java/json-simple-example-read-and-write-json/ and chatGPT aswell, can run and test it on the main

---
+++ src/entities/Target.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Target {
+
+    private Room room;
+    private String type;
+
+    public Target(Room room, String type) {
+        this.room = room;
+        this.type = type;
+    }
+}
---
+++ src/lib/interfaces/OrderedListADT.java
@@ -0,0 +1,19 @@
+package lib.interfaces;/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+
+/**
+ *
+ * @author pedro
+ */
+public interface OrderedListADT<T> extends ListADT<T> {
+
+    /**
+     * Adds the specified element to this list at the proper location
+     *
+     * @param element the element to be added to this list
+     */
+    public void add(T element);
+}
