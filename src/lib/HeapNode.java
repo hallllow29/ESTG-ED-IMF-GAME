468a4284f23abe8cbfc3f9a9044a5659b7b9fa27
hallllow29 <8230068@estg.ipp.pt>
2024-12-03 23:58:13 +0000

    LinkedOrderedList LinkedUnorderedList done

---
+++ src/lib/HeapNode.java
@@ -0,0 +1,17 @@
+package lib;
+
+import lib.interfaces.BinaryTreeADT;
+
+public class HeapNode<T> extends BinaryTreeADT<T> {
+    protected HeapNode<T> parent;
+    /**
+     * Creates a new heap node with the specified data.
+     *
+     * @param obj the data to be contained within
+     * the new heap nodes
+     */
+    public HeapNode (T obj) {
+        super(obj);
+        parent = null;
+    }
+}
